702641c086b4e96417234b041ab7c11e0d375c78
PauMellado <paulina.mellado.mateos@gmail.com>
2022-08-24 15:47:55 -0500

    Se agregaron nuevas Imágenes, se le dio la funcionalidad al botón para confirmar igualdad en Password.

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,9 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passWord: UITextField!
+    @IBOutlet weak var conPassword: UITextField!
+   
+    @IBOutlet weak var texCon: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        //Opcional
+        var optionalInteger: Int?// Puede o no puede tener un valor
+        optionalInteger = 1
+        print("Opcional: ", optionalInteger!)
+        // UNWRAP (DESENVOLVER)
+        //FORCE UNWRAP  print("Opcional: ", optionalInteger!) SE USA CUANDO SE ESTA COMPLETAMENTE SEGURO QUE LA VARIABLE TRAE VALOR
+        //SAFE UNWRAP
+
+        var color = semaforo(name: "azul")
+
+
+        if let unwraooedColor = color {
+            print("Exito", unwraooedColor)
+        }else{
+            print("Error")
+        }
+
+        guard let otroColor = color else { return }
+        print(otroColor)
+        
+        var nombre = greet(persona: "Paulina")
+        
+    }
+    
+    @IBAction func colorChange(_ sender: Any) {
+        if passWord.text == conPassword.text {
+            print("Contraseñas Iguales")
+            texCon.text = "Los password coinciden"
+            texCon.textColor = UIColor.green
+            
+        }else{
+            texCon.text = "Las password no coinciden"
+            texCon.textColor = UIColor.red
+        }
+    }
+    
+    func semaforo(name: String) -> String? {
+        if name == "rojo" {return "rojo"}
+        if name == "amarillo" {return "amarillo"}
+        if name == "verde" {return "verde"}
+        
+        
+        return nil
+    }
+    
+    func greet(persona person: String) -> String {
+        let greeting = "Hello " + person + "!"
+        return greeting
     }
 
 
